3bc81a5326650cf18bd2c14125818e2cadb8228c
WisdomHove <hovewisdom@gmail.com>
2021-07-13 15:10:51 +0200

    Gooogle Play Review(changing the SMS Activity, removing SMS permissions)

--- app/src/main/java/capenexis/co/za/gbvsms/MainActivity.java
+++ app/src/main/java/capenexis/co/za/gbvsms/MainActivity.java
@@ -3,9 +3,11 @@ package capenexis.co.za.gbvsms;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
